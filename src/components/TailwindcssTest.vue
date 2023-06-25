599e91bddeb2110db08ed3cd40971933174ea8d4
mythologica <pgmmind@gmail.com>
2023-06-25 13:30:38 +0900

    init

---
+++ src/components/TailwindcssTest.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <div class="space-y-4">
+        <div class="w-96 bg-purple-400 shadow rounded">
+            w-96
+        </div>
+        <div class="w-80 bg-purple-400 shadow rounded">
+            w-80
+        </div>
+        <div class="w-72 bg-purple-400 shadow rounded">
+            w-72
+        </div>
+        <div class="w-64 bg-purple-400 shadow rounded">
+            w-64
+        </div>
+        <div class="w-60 bg-purple-400 shadow rounded">
+            w-60
+        </div>
+        <div class="w-56 bg-purple-400 shadow rounded">
+            w-56
+        </div>
+    </div>
+</template>    
